781e447dddb76ca39abba6bbba4cba9dd6e1088f
Dalienst Owino <oduordalienst@gmail.com>
2026-01-13 01:36:51 +0300

    identify-error

--- forms/divisions/CreateDivisionModal.tsx
+++ forms/divisions/CreateDivisionModal.tsx
@@ -41,6 +41,7 @@ export default function CreateDivisionModal() {
 
         setOpen(false);
       } catch (error) {
+        console.log(error);
         const errorMessage =
           (error as any)?.response?.data?.message ||
           "Failed to create division";
